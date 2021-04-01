3f4624bd682ec1fc159eb3322a57514ddae716bc
akerestecioglu <akerestecioglu@sabanciuniv.edu>
2021-04-01 15:38:25 +0300

    removed unnecessary code lines

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from project import app, db
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, login_required, logout_user, current_user
@@ -6,7 +7,7 @@ from project.forms import LoginForm
 import os
 import portpicker
 
-
+# forgot password view
 @app.route('/forgot_password')
 def forgot_password():
     return render_template("forgot_password.html")
@@ -28,31 +29,19 @@ def welcome_user():
     # check if the user has an active prodigy process
     if current_user.has_active_process():
         return redirect('http://vpa-com16.ddns.sabanciuniv.edu:{}/?session={}'.format(current_user.used_port, current_user.username), code=302)
-    # burada ogrencinin girecegi portu almak gerekiyor
     os.environ['PRODIGY_HOST'] = "0.0.0.0"
-    # port 0 olursa otomatik olarak bos porta atabilir
+    # pick an available port to start the prodigy process
     available_port = int(portpicker.pick_unused_port())
     os.environ['PRODIGY_PORT'] = str(available_port)
 
-    # db isimleri $username-ner-db-01 okunacak dosya da $username-ner-01.jsonl
+    # start prodigy
     os.system('prodigy ner.manual {}-ner-db-01 blank:en ./{}-ner-01.jsonl --label PERSON,ORG,PRODUCT,LOCATION &'.format(current_user.username, current_user.username))
-    # os.system('prodigy ner.manual eagaev-ner-db-01 blank:en ./eagaev-ner-01.jsonl --label PERSON,ORG,PRODUCT,LOCATION &')
 
-    os.system('echo {} >> out'.format(current_user.username))
-    os.system('ps -u reyyan >> out')
-    # prodigy.serve("ner.manual ner_news_headlines blank:en ./news_headlines.jsonl --label PERSON,ORG,PRODUCT,LOCATION", port=available_port)
-
-    # save the port, process_id
+    # save the port information to db
     current_user.update_last_process(available_port)
     db.session.commit()
 
     return redirect('http://vpa-com16.ddns.sabanciuniv.edu:{}/?session={}'.format(available_port, current_user.username), code=302)
-    # return render_template('welcome_user.html')
-
-
-# user giris yaptiktan sonra ona ait active process varsa ona yonlendir, yoksa usttekini yap ve id yi ve time stampi db ye kaydet
-# pythondan process id ye nasıl ulasilir bak
-# 1 saati dolduran tum processleri oldur
 
 
 # login view
@@ -60,11 +49,10 @@ def welcome_user():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        # grab the user from our User Models table
+        # grab the user instance
         user = User.query.filter_by(username=form.username.data).first()
 
-        # check that the user was supplied and the password is right
-        # the check_password method comes from the User object
+        # check if the user exists and the password is right
         if user is not None and user.check_password(form.password.data):
             # log in the user
             login_user(user)
@@ -76,11 +64,12 @@ def login():
                 next_page = url_for('welcome_user')
 
             return redirect(next_page)
+        # show an error message
         flash(u'Invalid Credentials!', 'error')
-
+    # redirect to the login page if the form is not submitted correctly
     return render_template('login.html', form=form)
 
 
 # main
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8000)
